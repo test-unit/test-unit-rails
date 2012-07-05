1d6ba3d222c543edc2a37e93720926ec2eb8a06a
Kouhei Sutou <kou@clear-code.com>
2012-07-05 22:09:51 +0900

    Support Bundler 1.2.0.pre.1

--- lib/test/unit/rails/test_help.rb
+++ lib/test/unit/rails/test_help.rb
@@ -16,9 +16,11 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "test/unit/rails/version"
+require "test/unit/rails"
 
-require "test/unit/active_support"
-require "test/unit/notify"
-require "test/unit/rr"
-require "test/unit/capybara"
+require "rails/test_help"
+
+require "capybara/rails"
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+end
