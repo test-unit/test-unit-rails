665168709f01e395b636533718e1eeaf50a515e3
Sutou Kouhei <kou@clear-code.com>
2019-07-11 12:26:10 +0900

    Bump version

--- lib/test/unit/rails/version.rb
+++ lib/test/unit/rails/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2018  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012-2019  Sutou Kouhei <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
 module Test
   module Unit
     module Rails
-      VERSION = "5.2.3"
+      VERSION = "6.0.0"
     end
   end
 end
